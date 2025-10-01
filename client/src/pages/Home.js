3bf8f8468c44377745d0d6112efe3f3cfc7b1248
Oliver Lopez <oliverberto@gmail.com>
2025-10-01 01:04:46 -0400

    fixed layout issues in AlbumSelector css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import AlbumSelector from '../components/AlbumSelector/index';
